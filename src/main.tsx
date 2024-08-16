ec6771c04b639fdd86b304a7b36911858bc5bcf1
JJtheJJpro <jjthejjpro@gmail.com>
2024-08-16 09:45:12 -0600

    possible buffer fix 2?

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
+import "buffer";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')!
