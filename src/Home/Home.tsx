f89bdbb6f1dbae44d813e88d9a2ef71339718cf2
JJtheJJpro <jjthejjpro@gmail.com>
2025-08-22 10:13:05 -0600

    quick fix and router dependency safe check

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -4,9 +4,7 @@ export default function Home() {
             <div className='main'>
                 <p className='title'>Welcome to JJ's website!</p>
                 <p className='text'>
-                    As you can tell, there is much to be done here...
-                    <br />
-                    There are a few tabs that you can explore.  Have fun as you will, idek.a
+                    Here's my discord server: <a href='https://discord.gg/uB3yDDsKqD'>https://discord.gg/uB3yDDsKqD</a>
                 </p>
             </div>
         </>
